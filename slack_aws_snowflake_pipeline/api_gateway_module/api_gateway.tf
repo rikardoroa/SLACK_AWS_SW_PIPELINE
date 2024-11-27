ab982ee057346d05819795e12493764f1e12ec46
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2024-11-27 01:45:51 -0500

    Update api_gateway.tf

--- slack_aws_snowflake_pipeline/api_gateway_module/api_gateway.tf
+++ slack_aws_snowflake_pipeline/api_gateway_module/api_gateway.tf
@@ -1,6 +1,6 @@
 # creating api gateway object
 resource "aws_api_gateway_rest_api" "slackWebhook" {
-  name = "slackWebhook"
+  name        = "slackWebhook"
   description = "Slack API connection"
   endpoint_configuration {
     types = ["REGIONAL"]
@@ -16,10 +16,10 @@ resource "aws_api_gateway_resource" "slackwebhookpath" {
 
 # creating api gateway method
 resource "aws_api_gateway_method" "slackwebhookmethod" {
-  authorization = "NONE"
+  authorization = "NONE" # Cambiado a NONE para permitir acceso sin autenticación
   http_method   = "POST"
   resource_id   = aws_api_gateway_resource.slackwebhookpath.id
-  rest_api_id   =  aws_api_gateway_rest_api.slackWebhook.id
+  rest_api_id   = aws_api_gateway_rest_api.slackWebhook.id
 }
 
 # creating api gateway response
@@ -43,19 +43,25 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = var.invoke_arn
 }
 
+# creating api gateway integration response
+resource "aws_api_gateway_integration_response" "integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.slackWebhook.id
+  resource_id = aws_api_gateway_resource.slackwebhookpath.id
+  http_method = aws_api_gateway_method.slackwebhookmethod.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+  response_templates = {
+    "application/json" = ""
+  }
+}
 
 # permission to invoke lambda from api gateway
 resource "aws_lambda_permission" "allow_apigateway" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = var.function_name 
+  function_name = var.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.slackWebhook.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.slackWebhook.execution_arn}/dev/*"
 }
-
-
-
-
 
 # Creating API Gateway deployment
 resource "aws_api_gateway_deployment" "slackwebhook_deployment" {
@@ -65,7 +71,8 @@ resource "aws_api_gateway_deployment" "slackwebhook_deployment" {
   depends_on = [
     aws_api_gateway_method.slackwebhookmethod,
     aws_api_gateway_integration.integration,
-    aws_api_gateway_method_response.response_200
+    aws_api_gateway_method_response.response_200,
+    aws_api_gateway_integration_response.integration_response_200
   ]
 }
 
@@ -74,7 +81,6 @@ resource "aws_api_gateway_stage" "slackwebhook_stage" {
   deployment_id = aws_api_gateway_deployment.slackwebhook_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.slackWebhook.id
   stage_name    = "dev"
-
 }
 
 # Method settings to customize caching and throttling
@@ -83,8 +89,8 @@ resource "aws_api_gateway_method_settings" "slackwebhook_method_settings" {
   stage_name   = aws_api_gateway_stage.slackwebhook_stage.stage_name
   method_path  = "*/*"
   settings {
-    cache_data_encrypted = false
-    cache_ttl_in_seconds = 0
+    cache_data_encrypted  = false
+    cache_ttl_in_seconds  = 0
     throttling_burst_limit = 500
     throttling_rate_limit  = 1000
   }
