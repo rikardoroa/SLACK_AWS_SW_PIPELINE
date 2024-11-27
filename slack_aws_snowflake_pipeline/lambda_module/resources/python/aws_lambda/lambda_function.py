530d676cec5020b457a27acc8de2be4f38cb902c
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2024-11-27 11:28:55 -0500

    Update lambda_function.py

--- slack_aws_snowflake_pipeline/lambda_module/resources/python/aws_lambda/lambda_function.py
+++ slack_aws_snowflake_pipeline/lambda_module/resources/python/aws_lambda/lambda_function.py
@@ -2,13 +2,13 @@ import json
 from slack_content import GetSlackFile
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 def lambda_handler(event, context):
     # getting the lambda event
     body = json.loads(event["body"])
-    logger.info(f"este es el body:{body}")
+    logger.info(f"this is the body:{body}")
     # Handle Slack URL verification
     if body.get('type') == 'url_verification':
         return {
@@ -22,7 +22,7 @@ def lambda_handler(event, context):
     # getting the the file_id
     file_id = body['event'].get('file_id')
    
-    logger.info(f"este es el file id:{file_id}")
+    logger.info(f"this is the file id:{file_id}")
     # validating if the file exists using the slack api response
     if file_id:
         get_slack_data.get_file_info(file_id)
