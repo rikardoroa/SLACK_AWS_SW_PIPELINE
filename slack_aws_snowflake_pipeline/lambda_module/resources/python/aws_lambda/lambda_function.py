b5382ad7529c1496c5ffdbfba1267b4846a04981
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2024-11-27 00:42:29 -0500

    Update lambda_function.py

--- slack_aws_snowflake_pipeline/lambda_module/resources/python/aws_lambda/lambda_function.py
+++ slack_aws_snowflake_pipeline/lambda_module/resources/python/aws_lambda/lambda_function.py
@@ -1,31 +1,32 @@
 import json
 from slack_content import GetSlackFile
 
-
 def lambda_handler(event, context):
+    
+    # sending a responde per event to slack api
+    return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"challenge": body.get("challenge")})
+        }
+
     # getting the lambda event
     body = json.loads(event["body"])
     
     # validating slack api response
     if body.get('type') == 'url_verification':
         get_slack_data = GetSlackFile()
-        # getting the file id from the api response
-        file_id = body['event'].get('file_id')
-
-       
+        # getting the event data safely
+        event_data = body.get('event', {})
+        
+        # getting the file id from the event
+        file_id = event_data.get('file_id')
         
         # validating if the file exists using the slack api response
         if file_id:
             get_slack_data.get_file_info(file_id)
             get_slack_data.get_file_url()
-            
-
-        return {
-            "statusCode": 200,
-            "headers": {"Content-Type": "application/json"},
-            "body": json.dumps({"challenge": body.get("challenge")})
-        }
-
+        
         
 
     else:
@@ -34,3 +35,5 @@ def lambda_handler(event, context):
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps({"error": "file_id is missing in the event"})
         }
+
+
