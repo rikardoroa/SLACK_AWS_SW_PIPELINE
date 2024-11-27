f7b77707d8a01f5743a2c89355c717c4d58d833a
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2024-11-27 09:55:33 -0500

    Update main.tf

--- slack_aws_snowflake_pipeline/main.tf
+++ slack_aws_snowflake_pipeline/main.tf
@@ -16,12 +16,10 @@ module "aws_lambda_utils" {
   source      = "./lambda_module"
   kms_key_arn = module.bucket_utils.kms_key_arn
   token       = var.token
-  depends_on  = [module.bucket_utils]
 }
 
 module "api_gateway_utils" {
   source        = "./api_gateway_module"
   invoke_arn    = module.aws_lambda_utils.lambda_function.invoke_arn
   function_name = module.aws_lambda_utils.lambda_function.function_name
-  depends_on    = [module.aws_lambda_utils]
 }
