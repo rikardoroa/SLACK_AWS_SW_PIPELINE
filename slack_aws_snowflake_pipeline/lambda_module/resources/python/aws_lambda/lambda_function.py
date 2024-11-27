e85cdf9725683456dba39b2c01c7c7f9d8b74b83
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2024-11-27 01:13:44 -0500

    Update lambda_function.py

--- slack_aws_snowflake_pipeline/lambda_module/resources/python/aws_lambda/lambda_function.py
+++ slack_aws_snowflake_pipeline/lambda_module/resources/python/aws_lambda/lambda_function.py
@@ -2,35 +2,34 @@ import json
 from slack_content import GetSlackFile
 
 def lambda_handler(event, context):
-    
-   
-
     # getting the lambda event
     body = json.loads(event["body"])
 
-    # sending a responde per event to slack api
-    return {
+    # Handle Slack URL verification
+    if body.get('type') == 'url_verification':
+        return {
             "statusCode": 200,
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps({"challenge": body.get("challenge")})
-    }
-    
-    # validating slack api response
-    if body.get('type') == 'url_verification':
-        get_slack_data = GetSlackFile()
-        # getting the event data safely
-        event_data = body.get('event', {})
-        
-        # getting the file id from the event
-        file_id = event_data.get('file_id')
-        
-        # validating if the file exists using the slack api response
-        if file_id:
-            get_slack_data.get_file_info(file_id)
-            get_slack_data.get_file_url()
-        
-        
+        }
 
+    # Handle other events here
+    get_slack_data = GetSlackFile()
+    # getting the event data safely
+    event_data = body.get('event', {})
+
+    # getting the file id from the event
+    file_id = event_data.get('file_id')
+
+    # validating if the file exists using the slack api response
+    if file_id:
+        get_slack_data.get_file_info(file_id)
+        get_slack_data.get_file_url()
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"message": "File processing initiated"})
+        }
     else:
         return {
             "statusCode": 400,
